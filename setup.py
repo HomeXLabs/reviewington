066b7e4812b5902d6a4d1ab1695234115433a85a
YoungJun Park <yjpark@homex.com>
2021-02-08 14:54:36 -0500

    feat: set up for pip distribution

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from distutils.core import setup
+
+
+setup(
+  name = 'Reviewington',         
+  packages = setuptools.find_packages(),
+  version = '0.0.1', 
+  license='MIT',    
+  description = 'Reviewington gives you wings when reviewing code', #change later
+  author = 'HomeX',
+  author_email = 'reviewington@homex.com', 
+  url = 'https://github.com/HomeXLabs/reviewington',
+  download_url = 'https://github.com/HomeXLabs/reviewington',
+  classifiers=[
+    'Programming Language :: Python :: 3',    
+    'License :: OSI Approved :: MIT License', 
+  ],
+    python_requires='>=3.6'
+)
