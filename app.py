f66b87f69c8ae6cd7f88b508437766ec91c90a0b
YoungJun Park <yjpark@homex.com>
2021-02-08 13:58:54 -0500

    chore: run black for src code

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template
 
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder="templates")
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html', example='yoyo')
+    return render_template("index.html", example="example variable")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(passthrough_errors=False)
-
