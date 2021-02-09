7f89ce84b2f1f584a9cf5d6659eb2f87d861f606
DeepeshHomex <75995267+DeepeshHomex@users.noreply.github.com>
2021-02-10 01:26:51 +0530

    Update entrypoint.py

--- .github/actions/process-review/entrypoint.py
+++ .github/actions/process-review/entrypoint.py
@@ -23,6 +23,7 @@ def review_comment_check(comment_body):
 def parse_review_comment(data, github):
     for comment in data:
         if "in_reply_to_id" not in comment:
+            print(comment["body"])
             response = review_comment_check(comment["body"])
             print(response)
             if not review_comment_check(comment["body"]):
