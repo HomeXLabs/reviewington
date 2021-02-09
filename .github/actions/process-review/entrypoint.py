78a1faa87545e79e09c6aab1199b41c4a018f5c6
DeepeshHomex <75995267+DeepeshHomex@users.noreply.github.com>
2021-02-09 12:58:12 -0500

    github action with comment on review

---
+++ .github/actions/process-review/entrypoint.py
@@ -0,0 +1,65 @@
+from github import Github
+import argparse
+import os
+import requests
+import json
+from urllib.parse import urlparse
+
+
+def setup_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--token", help="A GitHub token for the repo")
+    parser.add_argument("--pr-number", help="get the PR number")
+
+    return parser.parse_args()
+
+def review_comment_check(comment_body):
+    res = comment_body.startswith(tuple(pref_list)) 
+    
+    return res
+
+def parse_review_comment(data):
+    for comment in data:
+        if "in_reply_to_id" not in comment:
+            if not review_comment_check(comment['body']):
+                review_comment_edit(comment['id'], github, comment['body'])
+
+    
+def review_comment_edit(id, github, body):
+    url = "https://api.github.com/repos/{}/pulls/comments/{}".format(repository_name, id)
+    print(url)
+    headers = { 'Accept': 'application/vnd.github.v3+json',
+              'Authorization': 'Bearer ' + str(github) }
+    print(headers)
+    payload = {'body': '⚠️  review etiquette not followed on : ' + str(body) + ' \n \n For more information please visit : https://github.com/HomeXLabs/reviewington/blob/main/docs/pr_etiquette.md '}
+    resp = requests.patch(url=url, headers=headers, data=json.dumps(payload))
+    
+    print(resp)
+    
+
+
+def main():
+    job_name = os.environ["GITHUB_WORKFLOW"]
+    global repository_name 
+    repository_name = os.environ["GITHUB_REPOSITORY"]
+    global pref_list
+    pref_list = ['Change','Question','Concern','Discussion','Praise','Suggestion','Nitpick','Guide','⚠️ ']
+    # setup arguments
+    args = setup_args()
+    github = args.token
+    global pr
+    pr = args.pr_number
+    print("Running in job %s on %s with pull number  %s" % (job_name, repository_name, args.pr_number))
+    url = "https://api.github.com/repos/{}/pulls/{}/comments".format(repository_name, pr)
+    
+    headers = { 'Accept': 'application/vnd.github.v3+json' }
+    
+    resp = requests.get(url=url, headers=headers)
+    data = resp.json()
+    parse_review_comment(data) 
+
+
+
+if __name__ == "__main__":
+    main()
+
