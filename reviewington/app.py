7369708d38f392b84c3c5a160636d711dde2ecd1
YoungJun Park <yjpark@homex.com>
2021-02-09 14:37:34 -0500

    feat: add second arg to cli for repo name

--- reviewington/app.py
+++ reviewington/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 
@@ -6,7 +7,8 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html", example="example variable")
+    repo_name = os.environ["GITHUB_ORG_REPO"]
+    return render_template("index.html", example=repo_name)
 
 
 if __name__ == "__main__":
