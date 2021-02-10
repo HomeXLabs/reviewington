b9c0af3561f4d60e63c724595179af6b38d2b182
YoungJun Park <yjpark@homex.com>
2021-02-10 16:18:58 -0500

    fix: to include required template files

--- setup.py
+++ setup.py
@@ -3,19 +3,21 @@ from distutils.core import setup
 
 
 setup(
-  name = 'reviewington',
-  packages = setuptools.find_packages(),
-  scripts = ['rton'],
-  version = '0.0.1', 
-  license='MIT',    
-  description = 'Reviewington gives you wings when reviewing code', #change later
-  author = 'HomeX',
-  author_email = 'reviewington@homex.com', 
-  url = 'https://github.com/HomeXLabs/reviewington',
-  download_url = 'https://github.com/HomeXLabs/reviewington',
-  classifiers=[
-    'Programming Language :: Python :: 3',    
-    'License :: OSI Approved :: MIT License', 
-  ],
-    python_requires='>=3.6'
+    name="reviewington",
+    packages=setuptools.find_packages(),
+    scripts=["rton"],
+    version="0.0.1",
+    license="MIT",
+    description="Reviewington gives you wings when reviewing code",  # change later
+    author="HomeX",
+    author_email="reviewington@homex.com",
+    url="https://github.com/HomeXLabs/reviewington",
+    download_url="https://github.com/HomeXLabs/reviewington",
+    package_dir={"reviewington": "reviewington"},
+    package_data={"reviewington": ["templates/*"], "reviewington": ["static/css/*"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
 )
