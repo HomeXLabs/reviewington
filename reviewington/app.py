e776f07b8612117884b3514c71faa7cdbb2a8f78
Joseph Winters <jwinters@homex.com>
2021-02-10 10:00:43 -0500

    Add default search query

--- reviewington/app.py
+++ reviewington/app.py
@@ -45,7 +45,7 @@ def getHtml(diffData, path):
 
 def searchMatch(params):
     filename = params.get("filename")
-    search_query = params.get("search")
+    search_query = params.get("search", "")
     missing_tag_allowed = False
     valid_tags = []
     for tag in TAGS:
