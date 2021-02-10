4163b15440f2659d6ae33670544ea2d4480e39fb
Joseph Winters <jwinters@homex.com>
2021-02-10 17:31:22 -0500

    Link to files

--- reviewington/app.py
+++ reviewington/app.py
@@ -19,12 +19,16 @@ def recurse_setdefault(res, array, currentPath):
     elif len(array) == 1:
         res[(array[0], os.path.join(currentPath, array[0]))] = {}
     else:
-        recurse_setdefault(res.setdefault((array[0], os.path.join(currentPath, array[0])), {}), array[1:], os.path.join(currentPath, array[0]))
+        recurse_setdefault(
+            res.setdefault((array[0], os.path.join(currentPath, array[0])), {}),
+            array[1:],
+            os.path.join(currentPath, array[0]),
+        )
 
 
 res = {}
 for f in out.decode("utf-8").splitlines():
-    recurse_setdefault(res, f.split("/"), '/')
+    recurse_setdefault(res, f.split("/"), "/")
 
 
 app = Flask(__name__, template_folder="templates")
@@ -45,6 +49,7 @@ TAGS = [
 
 def getHtml(diffData, path):
     return diff2html(diffData, path)
+
 
 def searchMatch(params):
     filepath = params.get("filepath", "")
@@ -81,6 +86,7 @@ def searchMatch(params):
 def index():
     return render_template("index.html", files=res)
 
+
 @app.route("/reviews", methods=["GET"])
 def reviews():
     return render_template(
@@ -92,6 +98,7 @@ def reviews():
                 {
                     "url": "https://api.github.com/repos/octocat/Hello-World/pulls/comments/1",
                     "path": "file1.txt",
+                    "urlToPath": "https://github.com/octocat/Hello-World/file1.txt",
                     "tag": "suggestion",
                     "diffHunk": getHtml(
                         "@@ -219,24 +239,77 @@\n     ```\n \n ## Ordering\n-  - Always follow the following order for methods in a React component:\n+\n+  - Ordering for class extends React.Component:\n+  \n+  1. constructor\n+  1. optional static methods\n+  1. getChildContext\n+  1. componentWillMount\n+  1. componentDidMount\n+  1. componentWillReceiveProps\n+  1. shouldComponentUpdate\n+  1. componentWillUpdate\n+  1. componentDidUpdate\n+  1. componentWillUnmount\n+  1. *clickHandlers or eventHandlers* like onClickSubmit() or onChangeDescription()\n+  1. *getter methods for render* like getSelectReason() or getFooterContent()\n+  1. *Optional render methods* like renderNavigation() or renderProfilePicture()\n+  1. render\n+\n+  - How to define propTypes, defaultProps, contextTypes, etc...  \n+\n+  ```javascript\n+  import React, { Component, PropTypes } from 'react';\n+  \n+  const propTypes = {\n+    id: PropTypes.number.isRequired,\n+    url: PropTypes.string.isRequired,\n+    text: PropTypes.string,\n+  };\n+  \n+  const defaultProps = {\n+    text: 'Hello World',\n+  };\n+  \n+  class Link extends Component {",
