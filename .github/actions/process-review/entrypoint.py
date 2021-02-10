0c99f7593d79582acf1fd2a1d986698c61cac81e
DeepeshHomex <75995267+DeepeshHomex@users.noreply.github.com>
2021-02-10 11:00:28 -0500

    updated comment pattern with difflib to remove exact match

--- .github/actions/process-review/entrypoint.py
+++ .github/actions/process-review/entrypoint.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import requests
 import json
+from difflib import get_close_matches
 from urllib.parse import urlparse
 
 
@@ -14,15 +15,27 @@ def setup_args():
     return parser.parse_args()
 
 
-def review_comment_check(comment_body):
 
-    return comment_body.startswith(tuple(pref_list))
+
+
+
+def fuzzy_review_comment_check(comment_body):
+    first_word = comment_body.split()[0]
+    print(first_word)
+    match = get_close_matches(first_word, pref_list, cutoff=0.8)
+    print(match)
+    if not match:
+        return False
+    else:
+        return True
+    
+
 
 
 def parse_review_comment(data, github):
     for comment in data:
         if "in_reply_to_id" not in comment:
-            if not review_comment_check(comment["body"]):
+            if not fuzzy_review_comment_check(comment["body"]):
                 review_comment_edit(comment["id"], github, comment["body"])
 
 
@@ -36,13 +49,11 @@ def review_comment_edit(id, github, body):
     }
     print(headers)
     payload = {
-        "body": "⚠️  review etiquette not followed on ⚠️  :  "
-        + str(body)
-        + " \n \n For more information please visit : https://github.com/HomeXLabs/reviewington/blob/main/docs/pr_etiquette.md "
+        "body": str(body) + " \n\n⚠️ [PR Comment Etiquette](https://github.com/HomeXLabs/reviewington/blob/main/docs/pr_etiquette.md) not followed on above comment ⚠️"
+
     }
     resp = requests.patch(url=url, headers=headers, data=json.dumps(payload))
 
-    print(resp)
 
 
 def main():
