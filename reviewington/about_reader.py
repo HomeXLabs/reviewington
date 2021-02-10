9749205b503dac2f7821d5b48e6edcefe9d72156
YoungJun Park <yjpark@homex.com>
2021-02-10 17:21:39 -0500

    fix: var

--- reviewington/about_reader.py
+++ reviewington/about_reader.py
@@ -2,7 +2,7 @@ import os
 
 
 current_dir, current_filename = os.path.split(__file__)
-about_path = os.path.join(this_dir, "about.txt")
+about_path = os.path.join(current_dir, "about.txt")
 
 f = open(about_path, "r")
 file_contents = f.read()
