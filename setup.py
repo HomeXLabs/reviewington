2276f46d7331f29d7fbb95c5fe41b831be7ea36e
Eric Jalbert <ericmjalbert@gmail.com>
2021-02-10 14:56:00 -0500

    fix: add missing dependencies to setup

--- setup.py
+++ setup.py
@@ -6,16 +6,21 @@ setup(
   name = 'reviewington',
   packages = setuptools.find_packages(),
   scripts = ['rton'],
-  version = '0.0.1', 
-  license='MIT',    
+  version = '0.0.1',
+  license='MIT',
   description = 'Reviewington gives you wings when reviewing code', #change later
   author = 'HomeX',
-  author_email = 'reviewington@homex.com', 
+  author_email = 'reviewington@homex.com',
   url = 'https://github.com/HomeXLabs/reviewington',
   download_url = 'https://github.com/HomeXLabs/reviewington',
   classifiers=[
-    'Programming Language :: Python :: 3',    
-    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
   ],
-    python_requires='>=3.6'
+  install_requires=[
+    "flask>=1.1",
+    "markdown>=3.3",
+    "pygithub>=1.54",
+  ],
+  python_requires='>=3.6'
 )
