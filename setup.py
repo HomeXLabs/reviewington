a1a8549bf4bba06923635a71ac837af178b0eb8b
YoungJun Park <yjpark@homex.com>
2021-02-11 13:26:29 -0500

    chore: setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     name="reviewington",
     packages=setuptools.find_packages(),
     scripts=["rton"],
-    version="0.0.1",
     license="MIT",
+    version="0.0.1",
     description="Reviewington gives you wings when reviewing code",  # change later
     author="HomeX",
     author_email="reviewington@homex.com",
@@ -26,5 +26,10 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
     ],
+    install_requires=[
+    "flask>=1.1",
+    "markdown>=3.3",
+    "pygithub>=1.54",
+  ],
     python_requires=">=3.6",
 )
